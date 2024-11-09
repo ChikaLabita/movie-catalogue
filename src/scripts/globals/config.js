ebe6d27b65dcfb26ca911e2169e62bf9fe786836
chikaLabita <itaaa.c2@gmail.com>
2024-11-09 20:19:13 +0700

    movie-catalogue : Application Shell, Web Manifest, Service Worker, Cache API.

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+    KEY: 'f49be60ed5fee074571f198e1ce716e9',
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+    DEFAULT_LANGUAGE: 'en-us',
+    CACHE_NAME: new Date().toISOString(),
+  };
+   
+  export default CONFIG;
